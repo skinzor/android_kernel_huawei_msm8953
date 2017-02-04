60be3fb6a40e95ab7f8e08d7ea95dd301df70fe7
Willi Ye <williye97@gmail.com>
2017-02-04 18:19:12 +0100

    Merge tag 'LA.UM.5.3-07710-89xx.0' of https://source.codeaurora.org/quic/la/kernel/msm-3.18 into caf2
    "LA.UM.5.3-07710-89xx.0"
    
    Used --ours for some of the conflicts.
    
    Conflicts:
    	arch/arm/boot/dts/qcom/msm-pmi8950.dtsi
    	arch/arm/boot/dts/qcom/msm8917-regulator.dtsi
    	arch/arm/boot/dts/qcom/msm8940.dtsi
    	drivers/media/platform/msm/camera_v2/isp/msm_isp.h
    	drivers/media/platform/msm/camera_v2/isp/msm_isp_axi_util.c
    	drivers/media/platform/msm/camera_v2/isp/msm_isp_util.c
    	drivers/misc/qcom/qdsp6v2/amrnb_in.c
    	drivers/misc/qseecom.c
    	drivers/platform/msm/usb_bam.c
    	drivers/video/msm/mdss/mdss_mdp.c
    	drivers/video/msm/mdss/mdss_mdp_wb.c
    	net/netfilter/xt_qtaguid.c
    	sound/soc/codecs/wcd-mbhc-v2.c
    	sound/soc/msm/qdsp6v2/msm-dolby-dap-config.c

--- sound/soc/msm/qdsp6v2/q6asm.c
+++ sound/soc/msm/qdsp6v2/q6asm.c
@@ -317,12 +317,12 @@ static void config_debug_fs_write(struct audio_buffer *ab)
 }
 static void config_debug_fs_init(void)
 {
-	out_buffer = kmalloc(OUT_BUFFER_SIZE, GFP_KERNEL);
+	out_buffer = kzalloc(OUT_BUFFER_SIZE, GFP_KERNEL);
 	if (out_buffer == NULL) {
 		pr_err("%s: kmalloc() for out_buffer failed\n", __func__);
 		goto outbuf_fail;
 	}
-	in_buffer = kmalloc(IN_BUFFER_SIZE, GFP_KERNEL);
+	in_buffer = kzalloc(IN_BUFFER_SIZE, GFP_KERNEL);
 	if (in_buffer == NULL) {
 		pr_err("%s: kmalloc() for in_buffer failed\n", __func__);
 		goto inbuf_fail;
@@ -2004,6 +2004,40 @@ void *q6asm_is_cpu_buf_avail(int dir, struct audio_client *ac, uint32_t *size,
 		return data;
 	}
 	return NULL;
+}
+
+int q6asm_cpu_buf_release(int dir, struct audio_client *ac)
+{
+	struct audio_port_data *port;
+	int ret = 0;
+	int idx;
+
+	if (!ac || ((dir != IN) && (dir != OUT))) {
+		pr_err("%s: ac %pK dir %d\n", __func__, ac, dir);
+		ret = -EINVAL;
+		goto exit;
+	}
+
+	if (ac->io_mode & SYNC_IO_MODE) {
+		port = &ac->port[dir];
+		mutex_lock(&port->lock);
+		idx = port->cpu_buf;
+		if (port->cpu_buf == 0) {
+			port->cpu_buf = port->max_buf_cnt - 1;
+		} else if (port->cpu_buf < port->max_buf_cnt) {
+			port->cpu_buf = port->cpu_buf - 1;
+		} else {
+			pr_err("%s: buffer index(%d) out of range\n",
+			       __func__, port->cpu_buf);
+			ret = -EINVAL;
+			mutex_unlock(&port->lock);
+			goto exit;
+		}
+		port->buf[port->cpu_buf].used = dir ^ 1;
+		mutex_unlock(&port->lock);
+	}
+exit:
+	return ret;
 }
 
 void *q6asm_is_cpu_buf_avail_nolock(int dir, struct audio_client *ac,
